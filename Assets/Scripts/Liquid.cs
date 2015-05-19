8ba9618aede66942e6a091fe39cffc6e26b9d317
Ben Cherrington <Cocanuta@gmail.com>
2015-05-19 20:44:26 +0100

    Added Liquid Manager

---
+++ Assets/Scripts/Liquid.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class Liquid{
+
+	public int id;
+	public string name;
+
+	public Liquid(int newID, string newName)
+	{
+		id = newID;
+		name = newName;
+	}
+}
---
+++ Assets/Scripts/LiquidManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LiquidManager : MonoBehaviour {
+
+	public List<Liquid> liquids = new List<Liquid>();
+	// Use this for initialization
+	void Start () {
+
+		AddNewLiquid ("Water");
+		AddNewLiquid ("Urine");
+		AddNewLiquid ("Liquid Poop");
+	
+	}
+	
+	void AddNewLiquid(string name)
+	{
+		int id = 0;
+		if (liquids.Count > 0) {
+			foreach(Liquid l in liquids)
+			{
+				if(l.id >= id)
+				{
+					id = l.id + 1;
+				}
+			}
+		}
+		liquids.Add (new Liquid (id, name));
+	}
+}
