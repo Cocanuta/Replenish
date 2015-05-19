218f8ca5a14a870414a03febf29397d096229df5
Smizzle92 <adz3192@googlemail.com>
2015-05-19 18:20:20 +0100

    Added Cauldron

---
+++ Assets/Scripts/Cauldron.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cauldron : MonoBehaviour {
+
+	public float cauldronSize;			//The size of the cauldron.
+	public float cauldronLevel;			//How full the cauldron is.
+	public Liquid cauldronLiquid;		//Type of liquid in the cauldron.
+	public Potion cauldronPotion;		//Type of potion in the cauldron.
+	public Materials cauldronMats;		//Materials that can be in the cauldron.
+
+	// Use this for initialization
+	void Start () {
+	//Set up the empty cauldron with the default size of 30L,
+		cauldronSize = 30000.0f;
+		cauldronLevel = 0.0f;
+		cauldronLiquid = Liquid.none;
+		cauldronMats = Materials.none;
+		cauldronPotion = cauldronPotion.none;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Empties the cauldron
+	void emptyCaldron () {
+		cauldronLevel = 0.0f;
+		cauldronLiquid = Liquid.none;
+		cauldronMats = Materials.none;
+		cauldronPotion = cauldronPotion.none;
+	}
+
+	//Fill the cauldron is liquid
+
+}
