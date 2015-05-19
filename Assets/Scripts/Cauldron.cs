76e943e7c72176ff4d93491c5bb2984332bca0ca
Smizzle92 <adz3192@googlemail.com>
2015-05-19 18:25:58 +0100

    Added cauldron comments

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Calls to constructor classes may need updated
+
 public class Cauldron : MonoBehaviour {
 
 	public float cauldronSize;			//The size of the cauldron.
@@ -14,9 +16,9 @@ public class Cauldron : MonoBehaviour {
 	//Set up the empty cauldron with the default size of 30L,
 		cauldronSize = 30000.0f;
 		cauldronLevel = 0.0f;
-		cauldronLiquid = Liquid.none;
-		cauldronMats = Materials.none;
-		cauldronPotion = cauldronPotion.none;
+		cauldronLiquid = Liquid.none; 		//May need changed once liquid constructor is created 
+		cauldronMats = Materials.none;		//May need changed once material constructor is created
+		cauldronPotion = Potion.none;		//May need changed once potion constructor is created 	
 	}
 	
 	// Update is called once per frame
