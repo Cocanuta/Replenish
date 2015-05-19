557d5d32a302f5c0b7417a4150824c7f623ac0de
Smizzle92 <adz3192@googlemail.com>
2015-05-19 22:23:32 +0100

    Liquid Manager

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -1,27 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 //Calls to constructor classes may need updated
 
 public class Cauldron : MonoBehaviour {
+	public LiquidManager liquidManager;
 
 	public float cauldronSize;			//The size of the cauldron.
 	public float cauldronLevel;			//How full the cauldron is.
 	public Liquid cauldronLiquid;		//Type of liquid in the cauldron.
 	public Potion cauldronPotion;		//Type of potion in the cauldron.
-	public Materials cauldronMats;		//Materials that can be in the cauldron.
-
-	public enum Liquid { none, water };
+	public Ingredients cauldronIngredients;		//Materials that can be in the cauldron.
+	
 	public enum Potion { none, health };
-	public enum Materials { none, plant };
+	public enum Ingredients { none, plant };
 
 	// Use this for initialization
 	void Start () {
 	//Set up the empty cauldron with the default size of 30L,
 		cauldronSize = 30000.0f;
 		cauldronLevel = 0.0f;
-		cauldronLiquid = Liquid.none;
-		cauldronMats = Materials.none;
+		cauldronLiquid = liquidManager.liquids[0];
+		cauldronIngredients = Ingredients.none;
 		cauldronPotion = Potion.none; 	
 	}
 	
@@ -34,7 +35,7 @@ public class Cauldron : MonoBehaviour {
 	void emptyCaldron () {
 		cauldronLevel = 0.0f;
 		cauldronLiquid = Liquid.none;
-		cauldronMats = Materials.none;
+		cauldronMats = Ingredients.none;
 		cauldronPotion = Potion.none;
 	}
 
