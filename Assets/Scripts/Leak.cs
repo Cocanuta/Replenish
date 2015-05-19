af0a7029d4e32b13a76677e95163246e5bf9c77c
David Engles <itsdavidengles@gmail.com>
2015-05-19 22:11:51 +0100

    Random leaks appear on roof.

---
+++ Assets/Scripts/Leak.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Leak : MonoBehaviour {
+
+	public float leakX;
+	public float leakY;
+	public float leakDuration;
+
+	public Leak (float x, float y, float duration)
+	{
+		leakX = x;
+		leakY = y;
+		leakDuration = duration;
+	}
+}
---
+++ Assets/Scripts/LeakManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class LeakManager : MonoBehaviour {
+
+	public Object leak;
+
+	public Transform left;
+	public Transform right;
+	public Transform top;
+	public Transform bottom;
+
+	public int leakCount = 3;
+
+	public bool isTimerRunning = false;
+
+	// Use this for initialization
+	void Start () {
+		createLeak();
+		createLeak();
+		createLeak();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void createLeak()
+	{
+		float x = (float)Random.Range (left.position.x+0.5f, right.position.x-0.5f);
+		float z = (float)Random.Range (bottom.position.z-0.5f, top.position.z+0.5f);
+		Object newLeak = Instantiate (leak, new Vector3 (x,6.0f, z), Quaternion.AngleAxis(-90.0f, Vector3.left));
+		StartCoroutine(leakTimer(newLeak));
+	}
+
+	IEnumerator leakTimer(Object leak)
+	{
+		yield return new WaitForSeconds(Random.Range(60,120));
+		Destroy(leak);
+		createLeak();
+	}
+}
